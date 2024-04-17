2818f53cadcbb84297a252b67bb181982609c305
Marcus Plaut <mr.plaut@gmail.com>
2024-04-17 16:20:11 -0500

    clicker game works with upgrade

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let s = 1
 let upgrade_clicks_amount = 10
 
 // how much cost to add
-let h = 10 * 1.1
+let h = upgrade_clicks_amount
 
 // Function to increment the score when the button is clicked
 function incrementScore() {
@@ -18,12 +18,34 @@ function incrementScore() {
 }
 
 function increaseClickerPower() {
-    console.log("Clicker power increased!");
+    console.log("Clicker power increased!")
 }
 
 function incrementHandUpgrade() {
-    upgrade_clicks_amount += h
-    // Update the required clicks
-    document.getElementById("hand").addEventListener("click", increaseClickerPower);
+    
+    if (score >= upgrade_clicks_amount) {
+        // minus the upgrade cost from score
+        score = score - upgrade_clicks_amount
 
+        // Update the score display
+        document.getElementById("score").textContent = score + ' clicks'
+        
+        // Recalculate the cost to add (h) based on the new upgrade_clicks_amount
+        h = Math.round(upgrade_clicks_amount * 1.66)
+
+        // Update the upgrade_clicks_amount
+        upgrade_clicks_amount += h
+
+        // Update the required clicks
+        document.getElementById("upgrade-required").textContent = "Cost: " + upgrade_clicks_amount + " clicks";
+
+        // update click power
+        s++
+        
+        // update html page with new click power
+        document.getElementById("clicker-power").textContent = "Click Power: " + s
+
+    }    
 }
+
+document.getElementById("hand").addEventListener("click", incrementHandUpgrade)
